bc4124abb9334ce8b40b163c8c2cf477268d58cf
Anita Safar <ancsa0416@gmail.com>
2021-11-26 14:07:11 +0000

    postlist created

---
+++ src/components/PostList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PostList = () => {
+    return(
+        <h2>This is the list</h2>
+    );
+};
+
+export default PostList;
--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -1,8 +1,12 @@
 import React from "react";
+import PostList from "../components/PostList";
 
 const PostsContainer = () => {
     return(
+        <>
         <h1>Reddit Newsfeed</h1>
+        <PostList/>
+        </>
     );
 };
 
